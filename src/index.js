028798d04929bb7e0e2b8add69e25418c2571c97
sowatever <ian781203@hotmail.com>
2018-06-16 14:41:05 -0700

    added redux dev tools for chrome extension

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import CreateForm from './Containers/DynamicForm/CreateForm';
 import registerServiceWorker from './registerServiceWorker';
 // import '../node_modules/popper.js/dist/popper'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import {CreateFormReducer} from './Reducers/DynamicForm/CreateForm'
+
+import { composeWithDevTools } from 'redux-devtools-extension'
 // import init from './Store'
 
 // import {headerNav} from './Containers/HeaderNav/action_reducer'
 
-const store = createStore(CreateFormReducer)
+const store = createStore(CreateFormReducer, composeWithDevTools(
+    applyMiddleware()))
 
 ReactDOM.render(
     <Provider store={store}>
